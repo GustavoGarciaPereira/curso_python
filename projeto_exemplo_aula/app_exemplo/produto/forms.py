844be476bd14f59d88a4b85663d745a6ab02c9dd
gustavogarciapereira <gusgurtavo@gmail.com>
2024-12-09 19:32:32 -0300

    up

--- projeto_exemplo_aula/app_exemplo/produto/forms.py
+++ projeto_exemplo_aula/app_exemplo/produto/forms.py
@@ -5,3 +5,13 @@ class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome', 'preco', 'estoque', 'descricao']
+
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
